619b425c3f6335f1ba150ee09775559925a325bf
Anesu Gandiwa <148047827+Anesugandiwa@users.noreply.github.com>
2025-04-15 23:37:21 +0200

    first commit

---
+++ resources/js/layouts/FooterLayout.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+<template>
+    <v-footer color="grey-lighten-3" app class="text-dark">
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <h4 class="font-weight-bold mb-2">Contact Us</h4>
+                    <span>Udugu Open House <br> 42 Harvey Brown Milton Park <br> Harare</span>
+
+                </v-col>
+                <v-col cols="12" md="4">
+                    <h4 class="font-weight-bold mb-2"> Subscribe To Our Newsletter</h4>
+                    <v-btn 
+                        prepend-icon="mdi-email"
+                        border-color = primary
+                        variant="flat"
+                        size="large"
+                    >
+                        SUBSCRIBE
+                    </v-btn>
+
+
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <h4 class="font-weight-bold mb-2">Join Us On Social Media</h4>
+                    <v-btn icon class="me-2" href="https://facebook.com" target="_blank">
+                        <v-icon>mdi-facebook</v-icon>
+                    </v-btn>
+                    <v-btn icon class="me-2" href="https://twitter.com" target="_blank">
+                        <v-icon>mdi-twitter</v-icon>
+                    </v-btn>
+                    <v-btn icon class="me-2" href="https://linkedin.com" target="_blank">
+                        <v-icon>mdi-linkedin</v-icon>
+                    </v-btn>
+                    <v-btn icon class="me-2" href="https://whatsapp.com" target="_blank">
+                        <v-icon>mdi-whatsapp</v-icon>
+                    </v-btn>
+
+                </v-col>
+            </v-row>
+        </v-container>
+
+    </v-footer>
+</template>
---
+++ resources/js/layouts/NavLayout.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <v-app> 
+    <v-app-bar color="grey-lighten-3" elevation="0">
+      <v-container>
+        <v-toolbar-items>
+          
+          <Link href="/">
+            <v-btn variant="text">Home</v-btn>
+          </Link>
+
+          
+          <Link href="/about">
+            <v-btn variant="text">About Us</v-btn>
+          </Link>
+
+          
+          <v-menu
+            open-on-hover
+            :open-delay="100"
+            :close-delay="200"
+            transition="slide-y-transition"
+          >
+          <template v-slot:activator="{ props }">
+            <v-btn 
+                variant="text"
+                v-bind="props"
+                append-icon="mdi-chevron-down"
+                class="text-capitalize"
+            >
+                Our Work
+            </v-btn>
+        </template>
+        <v-list density="compact" min-width="200">
+            <Link href="/education">
+                <v-list-item 
+                    title="Education"
+                    prepend-icon="mdi-school"
+                    value="education"
+                ></v-list-item>
+            </Link>
+            <Link href="/innovation">
+                <v-list-item 
+                    title="Innovation"
+                    prepend-icon="mdi-lightbulb-on"
+                    value="innovation"
+                ></v-list-item>
+            </Link>
+            <Link href="/policy">
+                <v-list-item 
+                    title="Policy"
+                    prepend-icon="mdi-file-document"
+                    value="policy"
+                ></v-list-item>
+            </Link>
+        </v-list>
+    </v-menu>
+    
+    <Link href="/publications">
+        <v-btn variant="text">Publications</v-btn>
+    </Link>
+    
+    <Link href="/acacia-talk">
+        <v-btn variant="text">Acacia Talk</v-btn>
+    </Link>
+    <Link href="/contact">
+        <v-btn variant="text">Contact Us</v-btn>
+    </Link>
+</v-toolbar-items>
+</v-container>
+</v-app-bar>
+<v-main>
+    <slot />
+</v-main>
+</v-app>
+</template>
